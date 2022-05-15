4ff5698382514d559d5a913772e7c51ea0b4ee3f
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-05-15 11:17:42 +0200

    👷 build an HTML version of the README

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
+# Standard Library
 import os
 from pathlib import Path
+
+# Third-Party Libraries
+import pandoc
 from plumbum import local, FG
 
 cwd = Path.cwd()
 
+# Build in subdirectories
 for build_py in cwd.glob("*/**/build.py"):
     dir = build_py.parent
     print(build_py)
@@ -14,5 +19,22 @@ for build_py in cwd.glob("*/**/build.py"):
         local["./build.py"] & FG
     finally:
         os.chdir(cwd)
+
+# Transform the README.md into a index.html
+with open("README.md", mode="r", encoding="utf-8") as file:
+    README = file.read()
+
+# Relative links (fork-friendly)
+README = README.replace("https://boisgera.github.io/python-fr/", "")
+doc = pandoc.read(README)
+options = [
+    "--standalone",
+    "--css=css/style.css",
+    "--include-in-header=html/font.html",
+    "--include-in-header=html/copy-code.html",
+    "--variable=lang:fr",
+    "--no-highlight"
+]
+pandoc.write(doc, file="index.html", format="html", options=options)
 
 
