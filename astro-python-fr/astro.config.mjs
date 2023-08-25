7da617f888b254fb57fbb939015e8245edc5e50b
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2023-08-25 18:58:42 +0200

    ✨ Astro stuff, admo, content, etc.

--- astro-python-fr/astro.config.mjs
+++ astro-python-fr/astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from "astro/config";
-
+import remarkToc from "remark-toc";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    assets: true,
+  },
   integrations: [
     mdx({
       syntaxHighlight: "shiki",
       shikiConfig: { theme: "github-dark" },
+      // Links don't work as expected
+      // remarkPlugins: [[remarkToc, { heading: "Table des matières" }]],
+      //
     }),
   ],
 });
